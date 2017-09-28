fcea41238f07d3b32cad21c9c87e5744429865f0
Richard Nienaber <rjnienaber@gmail.com>
2017-09-28 08:48:27 +0100

    add support for more types in parser

--- src/benchmark/parser.go
+++ src/benchmark/parser.go
@@ -2,38 +2,113 @@ package benchmark
 
 import (
 	"fmt"
+	"io"
 	"strings"
 )
 
-// IOReader bridges gap between a Buffer and Reader types
+// IOReader bridges gap between Buffer and Reader types
 type IOReader interface {
+	io.Reader
+	io.ByteReader
 	ReadString(delim byte) (string, error)
-	ReadByte() (byte, error)
 }
 
 func throwReadError(err error) {
 	panic(fmt.Sprintf("Couldn't read response: %v", err))
 }
 
-// Parse parses responses from Redis server
-func Parse(reader IOReader) string {
-	char, err := reader.ReadByte()
+func readByte(reader io.ByteReader) byte {
+	value, err := reader.ReadByte()
 	if err != nil {
 		throwReadError(err)
 	}
+	return value
+}
 
-	var result string
+func readBytes(n int, reader io.Reader) (bytesRead int, bytes []byte) {
+	var err error
+	bytes = make([]byte, n)
+	bytesRead, err = reader.Read(bytes)
+	if err != nil {
+		throwReadError(err)
+	}
+	return
+}
+
+func parseInteger(reader io.ByteReader) int {
+	number := 0
+	negativeMultipler := 1
+	for {
+		value := readByte(reader)
+		if value == 45 {
+			negativeMultipler = -1
+			continue
+		}
+
+		if value == 13 {
+			break
+		}
+
+		number = (number * 10) + int((value - 48))
+	}
+	readByte(reader) // discard '\n'
+	return number * negativeMultipler
+}
+
+func parseBulkString(reader IOReader) interface{} {
+	length := parseInteger(reader)
+	if length == 0 {
+		readBytes(2, reader)
+		return ""
+	}
+	if length == -1 {
+		return nil
+	}
+
+	bytesRead, bytes := readBytes(length, reader)
+	if bytesRead != length {
+		panic(fmt.Sprintf("Read less bytes than required (should have been %d, was %d)", length, readBytes))
+	}
+	return string(bytes[:bytesRead])
+}
+
+func parseArray(reader IOReader) interface{} {
+	length := parseInteger(reader)
+	if length == 0 {
+		var empty []interface{}
+		return empty
+	}
+	if length == -1 {
+		return nil
+	}
+
+	values := make([]interface{}, length)
+	for index := range values {
+		values[index] = Parse(reader)
+		readBytes(2, reader)
+	}
+
+	return values
+}
+
+// Parse parses responses from Redis server
+func Parse(reader IOReader) interface{} {
+	char := readByte(reader)
+
 	switch char {
-	case 43:
-		if result, err = reader.ReadString('\n'); err != nil {
+	case 36:
+		return parseBulkString(reader)
+	case 42:
+		return parseArray(reader)
+	case 43, 45:
+		result, err := reader.ReadString('\n')
+		if err != nil {
 			throwReadError(err)
 		}
-	case 45:
-		if result, err = reader.ReadString('\n'); err != nil {
-			throwReadError(err)
-		}
+		return strings.TrimSpace(result)
+	case 58:
+		return parseInteger(reader)
 	default:
 		panic(fmt.Sprintf("Response symbole '%v' not supported", char))
 	}
-	return strings.TrimSpace(result)
 }
--- src/benchmark/tests/parser_test.go
+++ src/benchmark/tests/parser_test.go
@@ -16,4 +16,77 @@ var _ = Describe("Parser", func() {
 	It("handles errors", func() {
 		Expect(benchmark.Parse(bytes.NewBufferString("-Error message\r\n"))).To(Equal("Error message"))
 	})
+
+	It("handles errors", func() {
+		Expect(benchmark.Parse(bytes.NewBufferString("$6\r\nfoobar\r\n"))).To(Equal("foobar"))
+	})
+
+	It("handles empty string", func() {
+		Expect(benchmark.Parse(bytes.NewBufferString("$0\r\n\r\n"))).To(Equal(""))
+	})
+
+	It("handles nil string", func() {
+		Expect(benchmark.Parse(bytes.NewBufferString("$-1\r\n"))).To(BeNil())
+	})
+
+	It("handles integers", func() {
+		Expect(benchmark.Parse(bytes.NewBufferString(":1000\r\n"))).To(Equal(1000))
+	})
+
+	It("handles empty array", func() {
+		var expected []interface{}
+		Expect(benchmark.Parse(bytes.NewBufferString("*0\r\n"))).To(Equal(expected))
+	})
+
+	It("handles string array", func() {
+		expected := []interface{}{"foo", "bar"}
+		Expect(benchmark.Parse(bytes.NewBufferString("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"))).To(Equal(expected))
+	})
+
+	// it('handles multiple empty arrays', () => {
+	// const buffer = new Buffer('*2\r\n*0\r\n*0\r\n');
+	// const values = parser.parse(buffer);
+	// assert.deepEqual(values, [[],[]]);
+	// });
+
+	// it('handles arrays of nil strings', () => {
+	// const buffer = new Buffer('*2\r\n$-1\r\n$-1\r\n');
+	// const values = parser.parse(buffer);
+	// assert.deepEqual(values, [undefined, undefined]);
+	// });
+
+	// it('handles arrays of empty strings', () => {
+	// const buffer = new Buffer('*2\r\n$0\r\n$0\r\n');
+	// assert.deepEqual(parser.parse(buffer), ['', '']);
+	// });
+
+	// it('handles arrays of integers', () => {
+	// const buffer = new Buffer('*3\r\n:1\r\n:2\r\n:3\r\n');
+	// assert.deepEqual(parser.parse(buffer), [1, 2, 3]);
+	// });
+
+	// it('handles mixed types', () => {
+	// const buffer = new Buffer('*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n');
+	// assert.deepEqual(parser.parse(buffer), [1, 2, 3, 4, 'foobar']);
+	// });
+
+	// it('handles nested arrays', () => {
+	// const buffer = new Buffer('*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*1\r\n+Foo\r\n');
+	// assert.deepEqual(parser.parse(buffer), [[1, 2, 3], ['Foo']]);
+	// });
+
+	// it('handles nested arrays with error', () => {
+	// const buffer = new Buffer('*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Foo\r\n-Bar\r\n');
+	// expect(() => parser.parse(buffer)).to.throw('Bar');
+	// });
+
+	// it('handles null arrays', () => {
+	// const buffer = new Buffer('*-1\r\n');
+	// assert.equal(parser.parse(buffer), undefined);
+	// });
+
+	// it('handles nulls in arrays', () => {
+	// const buffer = new Buffer('*3\r\n$3\r\nfoo\r\n$-1\r\n$3\r\nbar\r\n');
+	// assert.deepEqual(parser.parse(buffer), ['foo', undefined, 'bar']);
+	// });
 })
