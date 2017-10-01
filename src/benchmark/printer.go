1e7ee6e630a55a4c7b21cf9500d599b8ad1a5421
Richard Nienaber <rjnienaber@gmail.com>
2017-10-02 00:53:55 +0100

    print results

--- src/benchmark/printer.go
+++ src/benchmark/printer.go
@@ -1,10 +1,62 @@
 package benchmark
 
 import (
+	"fmt"
+	"math"
+	"sort"
 	"time"
 )
 
-func ShowThroughput(start time.Time, counter uint64) float64 {
-	elapsed := time.Now().Sub(start)
-	return float64(counter) / elapsed.Seconds()
+type Results struct {
+	Command       string
+	Requests      int
+	Elapsed       time.Duration
+	Connections   int
+	ResponseTimes map[int]int
+}
+
+// PrintThroughput prints the realtime throughput of the benchmark
+func PrintThroughput(start time.Time, counter *uint64) chan bool {
+	ticker := time.NewTicker(time.Millisecond * 250)
+	stop := make(chan bool, 1)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				throughput := float64(*counter) / time.Since(start).Seconds()
+				fmt.Printf("\rPING: %0.2f", throughput)
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	return stop
+}
+
+// PrintResults prints the final results for benchmark
+func PrintResults(results Results) {
+	fmt.Printf("\r%-100v\r", "") // clear last line
+	fmt.Printf("====== %s ======\n", results.Command)
+	fmt.Printf("  %d requests completed in %0.2f seconds\n", results.Requests, results.Elapsed.Seconds())
+	fmt.Printf("  %v concurrent clients\n", results.Connections)
+	fmt.Println("  3 bytes payload")
+	fmt.Println("  keep alive: 1")
+	fmt.Println()
+
+	times := make([]int, 0)
+	for k := range results.ResponseTimes {
+		times = append(times, k)
+	}
+	sort.Ints(times)
+	runningTotal := 0
+	for _, k := range times {
+		runningTotal += results.ResponseTimes[k]
+		percentage := math.Floor((float64(runningTotal)/float64(results.Requests))*10000.0) / 100.0
+		fmt.Printf("%0.2f%% <= %d milliseconds\n", percentage, k)
+	}
+
+	throughput := float64(results.Requests) / results.Elapsed.Seconds()
+	fmt.Printf("%0.2f requests per second\n", throughput)
+	fmt.Println()
 }
--- src/main/main.go
+++ src/main/main.go
@@ -11,6 +11,11 @@ import (
 	"time"
 )
 
+type BenchmarkRun struct {
+	Start time.Time
+	End   time.Time
+}
+
 func main() {
 	var options = benchmark.ProcessArguments()
 	if options.ShowHelp {
@@ -18,10 +23,20 @@ func main() {
 		os.Exit(1)
 	}
 	var wg sync.WaitGroup
+	var benchmarkWg sync.WaitGroup
 	start := time.Now()
 	var counter uint64
 	var limit uint64 = uint64(options.Requests)
+	results := make(chan BenchmarkRun)
+	benchmarkResults := benchmark.Results{
+		Command:     "PING",
+		Requests:    options.Requests,
+		Connections: options.Connections,
+	}
+	benchmarkResults.ResponseTimes = make(map[int]int)
+
 	for i := 0; i < options.Connections; i++ {
+		benchmarkWg.Add(1)
 		go func() {
 			conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", options.Host, options.Port))
 			if err != nil {
@@ -29,8 +44,15 @@ func main() {
 			}
 
 			for {
+				if atomic.AddUint64(&counter, 1) > limit {
+					benchmarkWg.Done()
+					break
+				}
+
+				runStart := time.Now()
 				fmt.Fprintf(conn, "PING\r\n")
 				result, err := benchmark.Parse(bufio.NewReader(conn))
+				run := BenchmarkRun{Start: runStart, End: time.Now()}
 				if err != nil {
 					panic(err)
 				}
@@ -39,30 +61,34 @@ func main() {
 					panic(fmt.Sprintf("Result should have been 'PONG' was '%v'", result))
 				}
 
-				if atomic.LoadUint64(&counter) == limit {
-					wg.Done()
-					break
-				}
-				atomic.AddUint64(&counter, 1)
+				results <- run
 			}
 		}()
-		wg.Add(1)
 	}
 
-	ticker := time.NewTicker(time.Millisecond * 250)
+	wg.Add(1)
 	go func() {
-		for _ = range ticker.C {
-			fmt.Printf("\rPING: %0.2f", benchmark.ShowThroughput(start, counter))
+		for run := range results {
+			ms := float64(run.End.Sub(run.Start).Nanoseconds()) / 1000000.0
+			if ms < 1 {
+				benchmarkResults.ResponseTimes[1] += 1
+			} else {
+				benchmarkResults.ResponseTimes[int(ms)] += 1
+			}
 		}
+		wg.Done()
 	}()
 
+	stopThroughput := benchmark.PrintThroughput(start, &counter)
+
+	benchmarkWg.Wait()
+	benchmarkResults.Elapsed = time.Since(start)
+	close(results)
+
+	stopThroughput <- true
 	wg.Wait()
-	ticker.Stop()
-	fmt.Printf("\r%-100v\r", "")
-	elapsed := time.Since(start)
-	fmt.Printf("time taken: %0.2fs\n", elapsed.Seconds())
-	fmt.Println("counter:", counter)
-	fmt.Printf("requests/sec: %0.2f\n", benchmark.ShowThroughput(start, counter))
+
+	benchmark.PrintResults(benchmarkResults)
 }
 
 // $ redis-benchmark
